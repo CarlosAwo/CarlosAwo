8d5e5d7cbcabf7a117369f2b1ebe3825f05b735b
Carlos <awocarlos55@gmail.com>
2024-05-28 10:42:12 +0100

    responsice

--- projets/e_commerce/src/components/CartComponent.vue
+++ projets/e_commerce/src/components/CartComponent.vue
@@ -1,44 +1,45 @@
 <template>
   <div class="container mx-auto py-6" v-if="cartItems.length > 0">
-  <h1 class="text-3xl mb-6">Shopping Cart 2  </h1>
-  <div class="flex flex-col">
-    <!-- Product Items -->
-    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-          <table class="min-w-full divide-y divide-gray-200">
-            <thead class="bg-gray-50">
-              <tr>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+    <h1 class="text-3xl mb-6">Shopping Cart 2</h1>
+    <div class="flex flex-col">
+      <!-- Product Items -->
+      <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table class="min-w-full divide-y divide-gray-200">
+              <thead class="bg-gray-50">
+                <tr>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     Product
-                </th>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  </th>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     Price
-                </th>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  </th>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     Quantity
-                </th>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  </th>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     Total
-                </th>
-              </tr>
-            </thead>
-            <tbody class="bg-white divide-y divide-gray-200">
-             <CartItemComponent v-for="prod, index in cartItems" :item="prod" />  
-            </tbody>
-          </table>
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="bg-white divide-y divide-gray-200">
+                <CartItemComponent v-for="prod, index in cartItems" :item="prod" :key="index" />
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
-    <!-- End of Product Items -->
-    <!-- Total Amount -->
-    <div class="mt-6 ml-auto">
+      <!-- End of Product Items -->
+      <!-- Total Amount -->
+      <div class="mt-6 lg:ml-auto lg:w-1/2">
         <div class="text-xl font-bold text-gray-900"> {{ total }} </div>
+      </div>
+      <!-- End of Total Amount -->
     </div>
-    <!-- End of Total Amount -->
   </div>
-</div>
 </template>
+
 
 <script setup>
 import { computed } from 'vue';
--- projets/e_commerce/src/components/CartItemComponent.vue
+++ projets/e_commerce/src/components/CartItemComponent.vue
@@ -1,26 +1,27 @@
 <template>
   <tr>
-    <td class="px-6 py-4 whitespace-nowrap">
+    <td class="px-4 md:px-6 py-2 md:py-4 whitespace-nowrap">
       <div class="flex items-center">
-        <div class="flex-shrink-0 h-10 w-10">
-          <img class="h-10 w-10 rounded-full" :src="item.image" alt="Product Image">
+        <div class="flex-shrink-0 h-8 md:h-10 w-8 md:w-10">
+          <img class="h-8 md:h-10 w-8 md:w-10 rounded-full" :src="item.image" alt="Product Image">
         </div>
-        <div class="ml-4">
-          <div class="text-sm font-medium text-gray-900"> {{item.description}} </div>
+        <div class="ml-2 md:ml-4">
+          <div class="text-xs md:text-sm font-medium text-gray-900">{{item.description}}</div>
         </div>
       </div>
     </td>
-    <td class="px-6 py-4 whitespace-nowrap">
-      <div class="text-left text-sm text-gray-900"> <span>$</span>{{ item.price }}</div>
+    <td class="px-4 md:px-6 py-2 md:py-4 whitespace-nowrap">
+      <div class="text-left text-xs md:text-sm text-gray-900"><span>$</span>{{item.price}}</div>
     </td>
-    <td class="text-left px-6 py-4 whitespace-nowrap">
-      <input @input="sayHello" type="number" class="text-black text-left w-16 px-3 py-1 border border-gray-300 rounded-md" v-model="item.qte">
+    <td class="px-4 md:px-6 py-2 md:py-4 whitespace-nowrap">
+      <input @input="sayHello" type="number" class="text-black text-left w-12 md:w-16 px-2 md:px-3 py-1 border border-gray-300 rounded-md" v-model="item.qte">
     </td>
-    <td class="text-left px-6 py-4 whitespace-nowrap">
-      <div class="text-left text-sm text-gray-900">$ {{ item.price * item.qte }} {{ item.qte }}</div>
+    <td class="px-4 md:px-6 py-2 md:py-4 whitespace-nowrap">
+      <div class="text-left text-xs md:text-sm text-gray-900">$ {{ item.price * item.qte }}</div>
     </td>
   </tr>
 </template>
+
 
 <script setup>
 import { defineProps, defineModel } from 'vue';
